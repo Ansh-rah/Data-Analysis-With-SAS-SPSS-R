aa8a80e6cf279b61f849802e0d99a6714703f734
Anshrah <shaikhanshrahcs242501@gmail.com>
2025-11-24 20:27:45 +0530

    Add files via upload

---
+++ practical 2/prac2.R
@@ -0,0 +1,19 @@
+read.csv("C:/Users/mvluc/OneDrive/Desktop/ANSHRAH/ADVANCE PYTHON FOR DATA SCIENCE/student_spending.csv", stringsAsFactors=TRUE)
+View(student_spending)
+lm(age~gender)
+attach(student_spending)
+
+
+
+
+read_excel("C:/Users/mvluc/OneDrive/Desktop/ANSHRAH/DATA ANALYSIS WITH SASS SPSS R/CHARACTERS_DATASET.xlsx")
+View(CHARACTERS_DATASET)
+lm(AGE~GENDER)
+attach(CHARACTERS_DATASET)
+
+
+
+read.csv("C:/Users/mvluc/OneDrive/Desktop/ANSHRAH/DATA ANALYSIS WITH SASS SPSS R/text_data.txt", stringsAsFactors=TRUE)
+View(text_data)
+lm(Age~Gender)
+attach(text_data)
